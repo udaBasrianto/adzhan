a5dc23578a88a49a107bd0de316e0911d66b8e93
udaBasrianto <128204449+udaBasrianto@users.noreply.github.com>
2025-04-13 06:21:17 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  ignores: [(message) => message.startsWith('chore(release)')],
+  rules: {
+    'footer-max-line-length': [0],
+  },
+};
